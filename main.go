80eac1ae5593675792df7bc7fab302b189975f04
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-29 00:37:03 -0300

    feat: gomail service provider

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"text/template"
 
 	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
 )
 
 func sendMailSMTP(subject string, message string, to []string) {
@@ -41,6 +42,11 @@ func sendMailHTML(subject string, templatePath string, to []string) {
 	t, err := template.ParseFiles(templatePath)
 	t.Execute(&body, struct{ Message string }{Message: "Hello from gbL"})
 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	auth := smtp.PlainAuth(
 		"",
 		os.Getenv("EMAIL"),
@@ -66,6 +72,36 @@ func sendMailHTML(subject string, templatePath string, to []string) {
 	fmt.Println("Email sent successfully!")
 }
 
+func sendGoMail(templatePath string) {
+	var body bytes.Buffer
+	t, err := template.ParseFiles(templatePath)
+	t.Execute(&body, struct{ Message string }{Message: "Hello from gbL"})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", os.Getenv("EMAIL"))
+	m.SetHeader("To", os.Getenv("EMAIL"))
+	m.SetHeader("Subject", "Hello!")
+	m.SetBody("text/html", body.String())
+	m.Attach("./dog.webp")
+
+	d := gomail.NewDialer("smtp.gmail.com",
+		587,
+		os.Getenv("EMAIL"),
+		os.Getenv("PASSWORD"),
+	)
+
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Email sent successfully!")
+}
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -74,15 +110,20 @@ func init() {
 }
 
 func main() {
+	// --- Using SMTP ---
 	// sendMailSMTP("Test",
 	// 	"This is a test email",
 	// 	[]string{
 	// 		os.Getenv("EMAIL"),
 	// 	})
 
-	sendMailHTML("golang SMTP Test",
-		"./test.html",
-		[]string{
-			os.Getenv("EMAIL"),
-		})
+	// --- Using SMTP + HTML ---
+	// sendMailHTML("golang SMTP Test",
+	// 	"./test.html",
+	// 	[]string{
+	// 		os.Getenv("EMAIL"),
+	// 	})
+
+	// --- Using GoMail ---
+	sendGoMail("./test.html")
 }
