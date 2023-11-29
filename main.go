7815faf5490f17d945cb7cf1221782783bd7e316
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-28 21:16:54 -0300

    chore: add godotenv

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	fmt.Println("Hello World!")
