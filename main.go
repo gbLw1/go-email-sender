fa72b45bd9642f20488929aca2826f272ebfcdd4
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-28 21:46:16 -0300

    feat: sending email with HTML templating string

--- main.go
+++ main.go
@@ -33,6 +33,32 @@ func sendMailSMTP(subject string, message string, to []string) {
 	fmt.Println("Email sent successfully!")
 }
 
+func sendMailHTML(subject string, html string, to []string) {
+	auth := smtp.PlainAuth(
+		"",
+		os.Getenv("EMAIL"),
+		os.Getenv("PASSWORD"),
+		"smtp.gmail.com",
+	)
+
+	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
+
+	msg := "Subject: " + subject + "\n" + headers + "\n\n" + html
+
+	err := smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		os.Getenv("EMAIL"),
+		to,
+		[]byte(msg),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Email sent successfully!")
+}
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -41,7 +67,15 @@ func init() {
 }
 
 func main() {
-	sendMailSMTP("Test", "This is a test email", []string{
-		os.Getenv("EMAIL"),
-	})
+	// sendMailSMTP("Test",
+	// 	"This is a test email",
+	// 	[]string{
+	// 		os.Getenv("EMAIL"),
+	// 	})
+
+	sendMailHTML("Test",
+		"<h1>This is a test email</h1><br/><p>With some html</p>",
+		[]string{
+			os.Getenv("EMAIL"),
+		})
 }
